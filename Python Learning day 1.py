64bd30529fe1f3b887ddda36c0b8f2fba81511b8
iamsourabh1402 <sourabhkumarvd@gmail.com>
2026-01-03 02:08:53 +0530

    day 1 of Python
    Posting my smallest codes here from Day 1

---
+++ Python Learning day 1.py
@@ -0,0 +1,8 @@
+
+# print ("Hello Sourabh, Lets begin")
+# calculate area of circle
+import math
+
+radius = float(input("Enter the radius of the circle: "))
+area = math.pi * math.pow(radius,2)
+print(f"The area of the circle with radius {radius} is: {round(area,2)}")
