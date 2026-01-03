16a3911789225b3adce748602d2df07da6f2737a
iamsourabh1402 <sourabhkumarvd@gmail.com>
2026-01-04 01:43:27 +0530

    Create Python Learning day 2.py

---
+++ Python Learning day 2.py
@@ -0,0 +1,27 @@
+
+# print ("Hello Sourabh, Lets begin")
+# calculate area of circle
+import math
+
+#radius = float(input("Enter the radius of the circle: "))
+#area = math.pi * math.pow(radius,2)
+#print(f"The area of the circle with radius {radius} is: {round(area,2)}")
+
+#lets build a simple calculator using If else
+num1 = int(input("Enter your first number "))
+num2 = int(input("Enter your second number ")) 
+operator = input("Enter operator (+, -, *, /): ")
+
+
+if operator == '+':
+    print (f"The sum of {num1} and {num2} is {num1 + num2}")
+elif operator == '-':
+    print (f"The difference of {num1} and {num2} is {num1-num2}")
+elif operator == '*':
+    print (f"The product of {num1} and {num2} is {num1 * num2}")
+elif operator == '/':
+    print (f"The division of {num1} and {num2} is {num1/num2}")
+elif operator == '%':
+    print (f"The modulous of {num1} and {num2} is {num1%num2}")
+else:
+    print ("invalid operator")
