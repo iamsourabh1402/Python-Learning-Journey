312adc9d754313c92e8f893d0c8c1693be2f85a1
iamsourabh1402 <sourabhkumarvd@gmail.com>
2026-01-06 03:14:26 +0530

    Create Python learning day 4.py
    learning operators today

---
+++ Python learning day 4.py
@@ -0,0 +1,10 @@
+# lets learn operators
+
+temp = int(input ("Enter temperature in Celsius: "))
+
+if temp <10 or temp > 40:
+     print ("Its a harsh day")
+elif temp > 10 and temp < 40 :
+    print ("Its a beautiful weather")
+else:
+    print ("Its Dooms Day")
