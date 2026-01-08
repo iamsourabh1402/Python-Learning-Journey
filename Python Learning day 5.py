e97fc7878d7a668b76443639f1d578d3dd32592d
iamsourabh1402 <sourabhkumarvd@gmail.com>
2026-01-08 10:02:45 +0530

    Create Python Learning day 5.py

---
+++ Python Learning day 5.py
@@ -0,0 +1,11 @@
+# lets learn some string methods in python
+text = "Hello, World!"
+print(text.upper())
+print(text.lower())
+print(text.strip())
+print(text.replace("World", "Python"))
+
+name = input ("enter your full name: ")
+print(len(name))
+result = name.rfind("L")
+print(result)
