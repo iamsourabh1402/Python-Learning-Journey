705fe2e07f1ec1625572bea2be2fec2932dca0b7
iamsourabh1402 <sourabhkumarvd@gmail.com>
2026-01-09 00:16:45 +0530

    Update Python Learning day 5.py

--- Python Learning day 5.py
+++ Python Learning day 5.py
@@ -1,11 +1,14 @@
 # lets learn some string methods in python
-text = "Hello, World!"
-print(text.upper())
-print(text.lower())
-print(text.strip())
-print(text.replace("World", "Python"))
+#   text = "Hello, World!"
+# print(text.upper())
+# print(text.lower())
 
-name = input ("enter your full name: ")
-print(len(name))
-result = name.rfind("L")
-print(result)
+# print(text.replace("World", "Python"))
+number = input ("enter your full number: ")
+# print(len(name))
+# result = name.rfind("L")
+# print(result)
+print(number.strip())
+
+#print(number.replace(" ", "+"))
+#print(number.count("8"  ))
