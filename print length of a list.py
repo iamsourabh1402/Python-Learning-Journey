8423d4c586f584ca5e78d694eb46d2314fbf5989
iamsourabh1402 <sourabhkumarvd@gmail.com>
2026-02-10 07:35:19 +0530

    Create print length of a list.py

---
+++ print length of a list.py
@@ -0,0 +1,14 @@
+fruits = ["apple", "banana", "cherry", "date", "elderberry"]
+veg = ["carrot", "broccoli", "spinach"]
+toys = ["lego", "barbie", "hot wheels", "action figure"]
+
+def list_len(list):
+    print (len(list))
+
+def print_list(list):
+    for item in list:
+        print(item, end=" " )
+
+
+
+print_list(fruits)
